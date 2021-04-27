fc7f4902f99e27d1f0762aebe865aa43c35ad5c5
Jasper van Merle <jaspervmerle@gmail.com>
2021-04-28 01:44:03 +0200

    Initial commit

---
+++ ALCPrinter/ALCPrinter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace ALCPrinter
+{
+    public class Printer
+    {
+        public static void PrintALC()
+        {
+            Console.WriteLine(AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly()));
+        }
+    }
+}
